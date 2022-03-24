32e83897ec68ea21713f613d6ab3c2f80d18df65
tornikeo <tornikeonoprishvili@gmail.com>
2022-03-24 16:07:04 +0400

    Fix workflow bug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ rm -rf frame-embed/
 rm -rf main/
 git config --global user.name 'GoodBot'
 git config --global user.email 'GoodBot@bots.noreply.github.com'
+git init
 git add .
 git commit -am "Auto update gh-pages"
-git push -f origin HEAD:gh-pages
+git push -force --allow-unrelated-histories origin HEAD:gh-pages
